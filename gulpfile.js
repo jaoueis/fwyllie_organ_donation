bca78963645848988c0a3ca94aff526116bbc90b
Shan Jiang <jiang_shan@live.com>
2018-03-06 01:00:19 -0500

    fully set up gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp        = require('gulp');
 var sass        = require('gulp-sass');
 var connect     = require('gulp-connect-php');
-var browserSync = require('browser-sync');
+var browserSync = require('browser-sync').create();
 
 function swallowError(error) {
     console.log(error.toString());
@@ -9,25 +9,29 @@ function swallowError(error) {
 }
 
 gulp.task('sass', function () {
-    return gulp.src(['sass/*.scss'])
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'sass/*.scss'])
                .pipe(sass())
                .on('error', swallowError)
                .pipe(gulp.dest('css/'))
                .pipe(browserSync.stream());
 });
 
+gulp.task('js', function () {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+               .pipe(gulp.dest('js/'))
+               .pipe(browserSync.stream());
+});
+
 gulp.task('serve', ['sass'], function () {
     connect.server({}, function () {
-        browserSync({
+        browserSync.init({
             injectChanges: true,
             proxy        : '127.0.0.1:8000'
         });
     });
 
-    gulp.watch(['sass/*.scss'], ['sass']);
-    gulp.watch('**/*.php').on('change', function () {
-        browserSync.reload();
-    });
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'sass/*.scss'], ['sass']);
+    gulp.watch(['*.php', 'section/*.html']).on('change', browserSync.reload);
 });
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['js', 'serve']);
--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>Document</title>
+<link rel="stylesheet" href="css/bootstrap.css">
 <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
@@ -15,5 +16,8 @@
 include('section/header.html');
 ?>
 
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
