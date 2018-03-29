a9e0204c33896ce258a3f5d51a876e3c2b1e0175
Shan Jiang <jiang_shan@live.com>
2018-03-29 15:28:14 -0400

    add body no scroll when open menu

--- js/mobile_nav.js
+++ js/mobile_nav.js
@@ -1,5 +1,6 @@
 $(function () {
     $('.burger-button').click(function () {
+        $('body').addClass('no-scroll');
         $('.main-nav-overlay').animate({right: 0}, 600, function () {
             $(this).addClass('animation');
             $('.mobile-nav').animate({opacity: 1, margin: 0}, 300);
@@ -10,5 +11,6 @@ $(function () {
         $('.main-nav-overlay').animate({right: '-100%'});
         $('.main-nav-overlay').removeClass('animation');
         $('.mobile-nav').animate({opacity: 0, margin: '-10px 0 0 0'}, 300);
+        $('body').removeClass('no-scroll');
     });
 });
