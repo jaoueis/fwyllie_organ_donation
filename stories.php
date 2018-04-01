b21a9ba604b76ccea7b44d73161768814858b640
Shan Jiang <jiang_shan@live.com>
2018-04-01 14:24:42 -0400

    set up stories page

---
+++ stories.php
@@ -0,0 +1,30 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Organ Donation</title>
+<link href="https://fonts.googleapis.com/css?family=Didact+Gothic|Rozha+One" rel="stylesheet">
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/font-awesome.min.css">
+<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+
+<?php
+include('section/header.html');
+include('section/stories_header_hero.html');
+include('section/stories_content.html');
+include('section/home_involved.html');
+include('section/footer.html');
+?>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/mobile_nav.js"></script>
+</body>
+</html>
